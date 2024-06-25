e99cb3285df5b87d46a254bfeb14932c1f44da1e
Mekhi-M <mekhim0731@gmail.com>
2024-06-25 11:44:34 -0400

    skeleton code: API test

--- egage-jokes/main.js
+++ egage-jokes/main.js
@@ -1,15 +1,24 @@
 import "./style.css";
 
 // API Test
-const apiTest = () => {
+const apiTest = async () => {
   const testRoute = async (url) => {
     const response = await fetch(url);
     const data = await response.json();
     console.log("data:", data);
+    return data;
   };
   const url1 = `https://official-joke-api.appspot.com/jokes/programming/ten`;
   const url2 = `https://official-joke-api.appspot.com/jokes/programming/random`;
-  testRoute(url1);
-  testRoute(url2);
+  const testMany = await testRoute(url1);
+  const testSingle = await testRoute(url2);
+
+  document.body.innerHTML += `
+  <p>${testSingle[0].setup} ${testSingle[0].punchline}</p>
+  <p>-------------------------------------------------</p>`;
+  for (let joke of testMany) {
+    document.body.innerHTML += `
+    <p>${joke.setup} ${joke.punchline}</p>`;
+  }
 };
 apiTest();
