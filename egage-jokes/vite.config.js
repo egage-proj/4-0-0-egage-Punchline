1e6b3cc4b1208b50e0784cd48da67e5bc95fce56
EngelsGB <engelsgarciabello@gmail.com>
2024-06-25 14:20:16 -0400

    Redeploy

--- egage-jokes/vite.config.js
+++ egage-jokes/vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   // GitHub Pages expects an index.html in the root directory
   // so just run npm build before pushing to GitHub and this will rebuild our assets to the root
-  build: { outDir: ".." },
+  build: { outDir: '..' },
   // needed for github pages just put the repo name here
-  base: "/4-0-0-egage-Punchline/",
+  base: '/4-0-0-egage-Punchline/', 
 });
