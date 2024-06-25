7460ae94eed060ae5a33d060ca6510ddf82df970
EngelsGB <engelsgarciabello@gmail.com>
2024-06-25 15:45:22 -0400

    Fully functional website (?)

--- egage-jokes/js/event-handlers.js
+++ egage-jokes/js/event-handlers.js
@@ -14,13 +14,23 @@ export const revealEvent = async (event) => {
         else if (event.target.textContent === 'Guess') {
             const fakes = await fetchJokeListByType();
             renderGame(joke[0], fakes[0]);
+            event.target.parentElement.innerHTML = `<p>${joke[0].setup}</p>`;
         };
     };
 }
 
 export const gameAnswer = async (event) => {
     if (event.target.matches('button')) {
+        const curJoke = event.target.parentElement.children[0].textContent;
         if (event.target.dataset.correct === 'true') document.querySelector('#guessGame').innerHTML = 'Correct!';
         else document.querySelector('#guessGame').innerHTML = 'Wrong!';
+        const li = document.querySelectorAll('li');
+        for (const element of li) {
+            if (element.children[0].textContent === curJoke) {
+                const joke = await fetchJokeID(element.dataset.jokeId);
+                element.innerHTML = `<p>${joke[0].setup} | ${joke[0].punchline}</p>`;
+            };
+        }
+        
     }
 }
