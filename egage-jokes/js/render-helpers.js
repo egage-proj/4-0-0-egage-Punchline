4f91a6b76ecdca715988a8296c3870e18389bfc7
EngelsGB <engelsgarciabello@gmail.com>
2024-06-25 15:20:20 -0400

    game draft

--- egage-jokes/js/render-helpers.js
+++ egage-jokes/js/render-helpers.js
@@ -8,11 +8,30 @@ export const renderJokes = (arrOfJokes) => {
         revealButton.textContent = 'Reveal';
         revealButton.dataset.jokeId = joke.id;
 
-        li.append(revealButton);
+        const guessButton = document.createElement('button');
+        guessButton.textContent = 'Guess';
+        guessButton.dataset.jokeId = joke.id;
+
+        li.append(guessButton, revealButton);
         document.querySelector('ul').append(li);
     }
 };
 
-export const renderGame = (jokeObj, arrOfJokes) => {
+export const renderGame = (jokeObj, jokeArr) => {
     const section = document.querySelector('#guessGame');
+    section.innerHTML = `<p>${jokeObj.setup}</p>`;
+    const right = Math.floor(Math.random() * 4);
+    for (let i = 0; i < 4; i++) {
+        const button = document.createElement('button');
+        button.classList.add('answers');
+        button.textContent = jokeArr[Math.floor(Math.random() * jokeArr.length)].punchline;
+        if (i === right) {
+            button.dataset.correct = true;
+            button.textContent = jokeObj.punchline;
+        } else {
+            button.dataset.correct = false;
+        }
+        section.append(button);
+    };
+    
 };
